151ecb8c46d0649d2c456b42ff9342d01f0eb314
Fabricio Dede <dede.exe@gmail.com>
2021-03-16 20:15:42 -0300

    add: Initial structure

---
+++ Project/GithubRepositories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubRepositories
+//
+//  Created by Fabricio do Santos on 16/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

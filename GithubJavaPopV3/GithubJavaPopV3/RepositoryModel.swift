879b1562c85f8d36e9bf0b1e4461b324dddda830
Suelen Critina Vaz <sucris12@gmail.com>
2022-07-05 15:52:16 -0300

    RepositoryStoryboard - Change color label and icon

--- GithubJavaPopV3/GithubJavaPopV3/RepositoryModel.swift
+++ GithubJavaPopV3/GithubJavaPopV3/RepositoryModel.swift
@@ -26,10 +26,6 @@ private func mockRepository() -> [Repository] {
     return [
         .fixture(),
         .fixture(),
-        .fixture(name: "Fabricio RObrigues Santos", description: "Geralmente são pessoas solícitas, que gostam de ajudar os outros e reunir os amigos. Seu senso de justiça também é bastante aflorado. As pessoas com ascendente em Libra podem ser um pouco narcisistas. Sua visão romantizada da vida costuma ser mais marcante, do que a de pessoas com sol no signo de Libra", stargazersCount: 23, forks: 656, owner: Owner.fixture()),
-        .fixture(),
-        .fixture(name: "Fabricio RObrigues Santos", description: "Geralmente são pessoas solícitas, que gostam de ajudar os outros e reunir os amigos. Seu senso de justiça também é bastante aflorado. As pessoas com ascendente em Libra podem ser um pouco narcisistas. Sua visão romantizada da vida costuma ser mais marcante, do que a de pessoas com sol no signo de Libra", stargazersCount: 23, forks: 656, owner: Owner.fixture()),
-        .fixture(),
-        .fixture()
+        .fixture(name: "Fabricio Robrigues Santos", description: "Geralmente são pessoas solícitas, que gostam de ajudar os outros e reunir os amigos. Seu senso de justiça também é bastante aflorado. As pessoas com ascendente em Libra podem ser um pouco narcisistas. Sua visão romantizada da vida costuma ser mais marcante, do que a de pessoas com sol no signo de Libra", stargazersCount: 23, forks: 656, owner: Owner.fixture(avatarUrl: "placeholder.png", login: "Dede.EXE"))
         ]
 }

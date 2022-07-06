61ac7964a28534adfcb55ae34a2b8135da53e1d7
Suelen Critina Vaz <sucris12@gmail.com>
2022-07-06 10:24:14 -0300

    Create Screen PullRequeste. PullRequesteStoryboard with viewController.

---
+++ GithubJavaPopV3/GithubJavaPopV3/PullRequestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PullRequestViewController.swift
+//  GithubJavaPopV3
+//
+//  Created by Suh on 06/07/22.
+//
+
+import UIKit
+
+class PullRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

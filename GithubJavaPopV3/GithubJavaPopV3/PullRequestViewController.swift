ccf4c21efe264a12eac10617a4126becd4f78323
Suelen Critina Vaz <sucris12@gmail.com>
2022-07-06 10:52:28 -0300

    Create outlets TableView and extension tableviewDataSource

--- GithubJavaPopV3/GithubJavaPopV3/PullRequestViewController.swift
+++ GithubJavaPopV3/GithubJavaPopV3/PullRequestViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class PullRequestViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = [
+    "Carlos",
+    "Julio",
+    "Pedro",
+    "Fernando",
+    "Natan"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension PullRequestViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+}
+
